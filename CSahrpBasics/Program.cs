ab9ce7bd5cad7313d1bb134f6704bd34a213054e
Khaled Eid <kheid@gmail.com>
2024-12-22 11:38:15 +0200

    gg

--- CSahrpBasics/Program.cs
+++ CSahrpBasics/Program.cs
@@ -9,6 +9,7 @@ namespace CSahrpBasics
     {
         static void Main(string[] args)
         {
+            //////////////////////////////////////////////////////
             #region Q 1 Write a program that allows the user to enter a number then print it.
 
             Console.Write("Enter a number: ");
