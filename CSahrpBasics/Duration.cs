7de0d35b3397ec15046749c23e6bf3128aa8b233
Khaled Eid <kheid@gmail.com>
2025-01-26 11:52:42 +0200

    Assignment Session06 OOP

---
+++ CSahrpBasics/Duration.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSahrpBasics
+{
+    public class Duration
+    {
+        int hours;
+        int minutes;
+        int seconds;
+
+        public int Hours { get; set; }
+        public int Minutes { get; set; }
+        public int Seconds { get; set; }
+
+        public Duration()
+        {
+        }
+
+        public Duration(int hours, int minutes, int seconds)
+        {
+            Hours = hours;
+            Minutes = minutes;
+            Seconds = seconds;
+        }
+        public Duration(int input) 
+        {
+            Hours = input / 3600;
+            input %= 3600;
+            Minutes = input / 60;
+            Seconds = input %= 60;
+        }
+
+        public override string ToString()
+        {
+            if (Hours == 0)
+            {
+                return $"Minutes:{Minutes},Seconds:{Seconds}";
+            }
+            return $"Hours:{Hours},Minutes:{Minutes},Seconds:{Seconds}";
+        }
+
+        public static Duration operator +(Duration a, Duration b)
+        {
+            return new Duration
+            {
+
+                Hours = a.Hours + b.Hours,
+                Minutes = a.Minutes + b.Minutes,
+                Seconds = a.Seconds + b.Seconds
+            };
+        }
+        public static Duration operator ++(Duration duration)
+        {
+            if (duration is not null)
+                return duration++;
+
+            return duration;
+            
+        }
+        public static Duration operator --(Duration duration)
+        {
+
+            if (duration is not null)
+                return duration--;
+
+            return duration;
+        }
+        public static Duration operator +(Duration a, int b)
+        {
+            return new Duration(a.Hours * 3600 + a.Minutes * 60 + a.Seconds + b);
+        }
+        public static Duration operator +(int a, Duration b)
+        {
+            return new Duration(b.Hours * 3600 + b.Minutes * 60 + b.Seconds + a);
+        }
+
+        public static Duration operator -(Duration a, Duration b)
+        {
+            return new Duration
+            {
+
+                Hours = a.Hours - b.Hours,
+                Minutes = a.Minutes - b.Minutes,
+                Seconds = a.Seconds - b.Seconds
+            };
+        }
+
+        public static bool operator >(Duration a, Duration b)
+        {
+            if (a.Hours == b.Hours)
+                return a?.Hours > b?.Hours;
+            else if (a.Minutes == b.Minutes)
+                return a?.Minutes > b?.Minutes;
+            else if (a.Seconds == b.Seconds)
+                return a?.Seconds > b?.Seconds;
+            else 
+                return b.Hours > a.Hours & b.Minutes > a.Minutes & a.Seconds > b.Seconds;
+
+        }
+        public static bool operator <(Duration a, Duration b)
+        {
+            if (a.Hours == b.Hours)
+                return a?.Hours < b?.Hours;
+            else if (a.Minutes == b.Minutes)
+                return a?.Minutes < b?.Minutes;
+            else if (a.Seconds == b.Seconds)
+                return a?.Seconds < b?.Seconds;
+            else
+                return b.Hours < a.Hours & b.Minutes < a.Minutes & a.Seconds < b.Seconds;
+
+        }
+
+        public static bool operator >=(Duration a, Duration b)
+        {
+            return (a.Hours * 3600 + a.Minutes * 60 + a.Seconds) >=
+                   (b.Hours * 3600 + b.Minutes * 60 + b.Seconds);
+        }
+
+        // Overload <= operator
+        public static bool operator <=(Duration a, Duration b)
+        {
+            return (a.Hours * 3600 + a.Minutes * 60 + a.Seconds) <=
+                   (b.Hours * 3600 + b.Minutes * 60 + b.Seconds);
+        }
+
+        public static implicit operator DateTime(Duration d)
+        {
+            return new DateTime().AddHours(d.Hours).AddMinutes(d.Minutes).AddSeconds(d.Seconds);
+        }
+    }
+    
+
+
+}
--- CSahrpBasics/Program.cs
+++ CSahrpBasics/Program.cs
@@ -9,8 +9,48 @@ namespace CSahrpBasics
     {
         static void Main(string[] args)
         {
-          
 
+            ////Duration D1 = new Duration(1,10,15);
+            ////Console.WriteLine(D1.ToString()); 
+
+            //Duration D1 = new Duration(3600);
+            //Console.WriteLine(D1.ToString());
+
+            //Duration D2 = new Duration(7800);
+            //Console.WriteLine(D2.ToString());
+
+            ////Duration D3 = new Duration(666);
+            ////Console.WriteLine(D3.ToString());
+
+            //Duration D3 = D1 + D2;
+            //Console.WriteLine(D3.ToString());
+
+            ////Duration d4 = D1 + 7800;
+            ////Console.WriteLine(d4.ToString());
+
+            ////Duration D5 = 666 + D3;
+            ////Console.WriteLine(D5.ToString());
+            ////D3 +=D1;
+            ////D3++;
+            ////Console.WriteLine(D3.ToString());
+
+            //D1 = D2 - D1;
+            //Console.WriteLine(D1.ToString());
+
+            //if (D1 > D2)
+            //    Console.WriteLine("D1 Greater than D2");
+            //else 
+            //    Console.WriteLine("D2 Greater than D1");
+
+
+            //if (D1 <= D2)
+            //    Console.WriteLine("D1 less than D2");
+            //else
+            //    Console.WriteLine("D1 Greater than D2");
+
+
+            //DateTime Obj = (DateTime)D1;
+            //Console.WriteLine(Obj);
 
 
 
