0f4dc460308bf342b91d3f04643f300ad896e56f
Khaled Eid <kheid@gmail.com>
2025-02-12 11:52:01 +0200

    Adv C# Session04 Assignment

---
+++ CSahrpBasics/Book.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSahrpBasics
+{
+    public class Book
+    {
+        public string ISBN { get; set; }
+        public string Title { get; set; }
+        public string Authors { get; set; }
+        public DateTime PublicationDate { get; set; }
+        public decimal Price { get; set; }
+
+        public Book(string _ISBN, string _Title, string _Authors, DateTime _PublicationDate, decimal _Price)
+        {
+            ISBN = _ISBN;
+            Title = _Title;
+            Authors = _Authors;
+            PublicationDate = _PublicationDate;
+            Price = _Price;
+        }
+
+        public override string? ToString()
+        {
+            return $"ISBN: {ISBN}, Title: {Title}, Authors: {Authors}, Publication Date: {PublicationDate.ToShortDateString()}, Price: {Price:C}";
+        }
+    }
+
+    public class BookFunctions
+    {
+       
+        public static string GetTitle(Book B)
+        {
+            return B.Title;
+        }
+
+        
+        public static string GetAuthors(Book B)
+        {
+            return B.Authors;
+        }
+
+        
+        public static string GetPrice(Book B)
+        {
+            return string.Join(" ", B.Price);
+        }
+    }
+}
